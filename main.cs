a3054688a93c481f1dafe8b6cde975dc3abf82ab
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-28 20:52:35 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+//
+// This program displays, "Salary Program, with JS"
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("You work 8 hours each day");
+      Console.WriteLine("You make $14 per hour");
+    Console.WriteLine("Your pay will be: $1836 monthly");
+      Console.WriteLine("The government will take: $403.20 monthly.");
+        Console.WriteLine("\nDone.");
+    }
+}
